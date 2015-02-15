11eef29fa8dd25b3617eb87da03cf9001aa21d46
Carlo Luis Bation <cbation@isbx.com>
2015-02-16 00:07:31 +0800

    Organise project structure.

--- InstagramApp/Controllers/InstagramMainViewController.m
+++ InstagramApp/Controllers/InstagramMainViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2015 incube8. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "InstagramMainViewController.h"
 
-@interface ViewController ()
+@interface InstagramMainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation InstagramMainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
