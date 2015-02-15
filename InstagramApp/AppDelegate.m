0f556ed4420e92563811ce880be20ae0af4a22f8
Carlo Luis Bation <cbation@isbx.com>
2015-02-16 00:23:47 +0800

    Implement basic login flow.

--- InstagramApp/AppDelegate.m
+++ InstagramApp/AppDelegate.m
@@ -21,11 +21,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    
+ 
     self.instagram = [[Instagram alloc] initWithClientId:APP_ID
                                                 delegate:nil];
     
--- InstagramApp/Controllers/InstagramMainViewController.m
+++ InstagramApp/Controllers/InstagramMainViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "InstagramMainViewController.h"
+#import "AppDelegate.h"
+#import "IGConnect.h"
 
-@interface InstagramMainViewController ()
+@interface InstagramMainViewController ()<IGSessionDelegate>
 
 @end
 
@@ -17,11 +19,69 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    AppDelegate* appDelegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+    
+    // here i can set accessToken received on previous login
+    appDelegate.instagram.accessToken = [[NSUserDefaults standardUserDefaults] objectForKey:@"accessToken"];
+    appDelegate.instagram.sessionDelegate = self;
+    if ([appDelegate.instagram isSessionValid]) {
+        //
+        
+    } else {
+        [appDelegate.instagram authorize:[NSArray arrayWithObjects:@"basic", nil]];
+    }
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)loginButtonPressed:(id)sender {
+    
+    AppDelegate* appDelegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+    [appDelegate.instagram authorize:[NSArray arrayWithObjects:@"basic", nil]];
+}
+
+
+
+#pragma - IGSessionDelegate
+
+-(void)igDidLogin {
+    NSLog(@"Instagram did login");
+    // here i can store accessToken
+    AppDelegate* appDelegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+    [[NSUserDefaults standardUserDefaults] setObject:appDelegate.instagram.accessToken forKey:@"accessToken"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+}
+
+-(void)igDidNotLogin:(BOOL)cancelled {
+    NSLog(@"Instagram did not login");
+    NSString* message = nil;
+    if (cancelled) {
+        message = @"Access cancelled!";
+    } else {
+        message = @"Access denied!";
+    }
+    UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil];
+    [alertView show];
+}
+
+-(void)igDidLogout {
+    NSLog(@"Instagram did logout");
+    // remove the accessToken
+    [[NSUserDefaults standardUserDefaults] setObject:nil forKey:@"accessToken"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+-(void)igSessionInvalidated {
+    NSLog(@"Instagram session was invalidated");
 }
 
 @end
